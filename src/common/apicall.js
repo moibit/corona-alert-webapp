ab095227280da2afdf055d6229a776b6b797f32b
its-VSP <prasanthaltruist4444@gmail.com>
2020-03-20 11:28:57 +0530

    utils that relay on external world

--- src/common/apicall.js
+++ src/common/apicall.js
@@ -1,24 +1,95 @@
+import Moi_ID from 'js-moi-id';
 import axios from 'axios';
-const url = "http://localhost:3019"
+import CryptoJS from "crypto-js";
+
+const moi_id = new Moi_ID();
+
 
 export const registeruser = async (data) => {
     try {
-        let response = await axios.post(url + "/user/create", data);
-        //return response;
-        console.log(response);
+        await moi_id.create(data.mobile,data.mobile);
+        return true
     }
-    catch (Err) {
-        console.log(Err);
+    catch(err) {
+        console.log(err);
     }
 }
 
-export const loginUser = async (data) => {
+export const loginuser = async (data) => {
     try {
-        let response = await axios.post(url + "/user/login", data);
-        //return response;
-        console.log(response)
+        const loginR = await moi_id.unlock(data.mobile,data.mobile);
+        window.devIndex = moi_id.getDeveloperIndex();
+        return loginR;
     }
-    catch (Err) {
-        console.log(Err);
+    catch (err) {
+        console.log(err);
     }
+}
+
+export const storeInMoiBit = async (data) => {
+    const devIndex = moi_id.getDeveloperIndex();
+    const authResponse = await axios({
+        url : 'http://localhost:3019/keyaccount/id/'+devIndex,
+        method : 'GET'
+    });
+    const config = authResponse.data.result;
+    const symmetricKey = await moi_id.getCipherKey();
+    let cipherText = CryptoJS.AES.encrypt(JSON.stringify(data),symmetricKey).toString();
+    const name = moi_id.getID();
+    const payload = {
+        fileName : name+'.txt',
+        text : cipherText,
+        create : 'true'
+    };
+    const tempUrl = 'https://kfs'+config.api_url+'.moibit.io/moibit/v0/writetexttofile';
+    return await axios({
+        url : tempUrl,
+        method : 'POST',
+        data: payload,
+        headers : {
+            'api_key' : config.api_key,
+            'api_secret' : config.api_secret
+        }
+    })
+}
+
+function decryptUD(encryptedKS,__passphrase) {
+    return new Promise((resolve,reject) => {
+        try {
+            var decryptedBytes  = CryptoJS.AES.decrypt(encryptedKS.toString(),__passphrase);
+            var decryptedData = JSON.parse(decryptedBytes.toString(CryptoJS.enc.Utf8));
+            resolve(decryptedData);
+        }catch(e) {
+            reject(e);
+        }  
+    })
+}
+
+export const getUserData = async () => {
+    try {
+        const name = moi_id.getID();
+        const devIndex = moi_id.getDeveloperIndex();
+        const authResponse = await axios({
+            url : 'http://localhost:3019/keyaccount/id/'+devIndex,
+            method : 'GET'
+        });
+        const config = authResponse.data.result;
+            const tempUrl = 'https://kfs'+config.api_url+'.moibit.io/moibit/v0/readfile';
+            const udR = await axios({
+                url : tempUrl,
+                method : 'POST',
+                data: {
+                    "fileName" : '/'+name+'.txt'
+                },
+                headers : {
+                    'api_key' : config.api_key,
+                    'api_secret' : config.api_secret
+                }
+            })
+            const symmetricKey = await moi_id.getCipherKey();
+            return await decryptUD(udR.data,symmetricKey);
+    }catch(e) {
+        return {};
+    }
+    
 }
